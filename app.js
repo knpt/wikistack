0c14bf52936f312c3687815ff5cbdac6b3b7cb1f
Mieka Page <git@coldwine.nyc>
2017-08-07 11:00:03 -0400

    sync database, then start server

--- app.js
+++ app.js
@@ -2,23 +2,27 @@ var morgan = require('morgan');
 var express = require('express');
 var bodyParser = require('body-parser');
 var nunjucks = require('nunjucks');
+var models = require('./models');
 
 var app = express();
 
-// point nunjucks to the directory containing templates and turn off caching; configure returns an Environment 
+// point nunjucks to the directory containing templates and turn off caching; configure returns an Environment
 // instance, which we'll want to use to add Markdown support later.
 var env = nunjucks.configure('views', {noCache: true});
 // have res.render work with html files
 app.set('view engine', 'html');
 // when res.render works with html files, have it use nunjucks to do so
 app.engine('html', nunjucks.render);
-
-// start the server
-var server = app.listen(1337, function(){
-    console.log("listening");
-});
-
 app.get('/', function(req, res, next){
     res.render('index')
-})
+});
 
+models.User.sync({})
+.then(function() {
+    return models.Page.sync({});
+})
+.then(function() {
+    app.listen(3000, function() {
+        console.log('Server is listening on port 3000!');
+    });
+});
